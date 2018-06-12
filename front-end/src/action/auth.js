ebbcd02d1f66d643cccefd90622a4c4c2da12b14
Mario Flores Jr <m.flores4821@gmail.com>
2018-06-11 21:57:18 -0700

    header and logout

--- front-end/src/action/auth.js
+++ front-end/src/action/auth.js
@@ -1,5 +1,7 @@
 import superagent from 'superagent';
 import * as route from '../utils/route';
+import { deleteCookie } from '../utils/cookie';
+import { TOKEN_COOKIE_KEY } from '../constants';
 
 
 export const setTokenAction = token => ({
@@ -11,10 +13,15 @@ export const removeTokenAction = () => ({
   type: 'TOKEN_REMOVE',
 });
 
+export const logout = () => {
+  deleteCookie(TOKEN_COOKIE_KEY);
+  return removeTokenAction();
+};
 
 export const signupRequest = user => (store) => {
   return superagent.post(`${API_URL}${route.SIGNUP_ROUTE}`)
     .send(user)
+    .withCredentials()
     .then((response) => {
       return store.dispatch(setTokenAction(response.text));
     });
@@ -24,6 +31,7 @@ export const signupRequest = user => (store) => {
 export const loginRequest = user => (store) => {
   return superagent.get(`${API_URL}${route.LOGIN_ROUTE}`)
     .auth(user.username, user.password)
+    .withCredentials()
     .then((response) => {
       return store.dispatch(setTokenAction(response.text));
     });
---
+++ front-end/src/component/profile-form/profile-form.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import autoBind from '../../utils';
+
+const defaultState = {
+
+};
+
+class ProfileForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = props.profile ? props.profile : defaultState;
+    autoBind.call(this, ProfileForm);
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.onComplete(this.state);
+  }
+
+  render() {
+    return (
+      <form
+        className='profile-form'
+        onSubmit={this.handleSubmit}>
+
+        <textarea
+          name='bio'
+          value={this.state.bio}
+          onChange={this.handleChange}
+        />
+
+        <button type='submit'> {this.props.profile ? 'update' : 'create'} profile </button>
+      </form>
+    );
+  }
+}
+
+ProfileForm.propTypes = {
+  onComplete: PropTypes.func,
+  profile: PropTypes.object,
+};
+
+export default ProfileForm;
