6896b5bcbc190e8278c8a0db1de4251490f1b22e
Mario Flores Jr <m.flores4821@gmail.com>
2018-06-14 01:21:07 -0700

    profile to combine reducers

--- front-end/src/action/profileAction.js
+++ front-end/src/action/profileAction.js
@@ -1,5 +1,5 @@
 import superagent from 'superagent';
-import * as route from '../utils/route';
+import * as routes from '../utils/route';
 
 const setProfile = profile => ({
   type: 'CLIENT_PROFILE_SET',
@@ -9,7 +9,7 @@ const setProfile = profile => ({
 const profileCreateRequest = profile => (store) => {
   const { token } = store.getState();
 
-  return superagent.post(`${API_URL}${route.PROFILE_ROUTE}`)
+  return superagent.post(`${API_URL}${routes.PROFILE_ROUTE}`)
     .set('Authorization', `Bearer ${token}`) 
     .set('Content-Type', 'application/json')
     .send(profile)
@@ -21,7 +21,7 @@ const profileCreateRequest = profile => (store) => {
 const profileUpdateRequest = profile => (store) => {
   const { token } = store.getState();
 
-  return superagent.put(`${API_URL}${route.PROFILE_ROUTE}/${profile._id}`)
+  return superagent.put(`${API_URL}${routes.PROFILE_ROUTE}/${profile._id}`)
     .set('Authorization', `Bearer ${token}`)
     .set('Content-Type', 'application/json')
     .send(profile)
@@ -33,7 +33,7 @@ const profileUpdateRequest = profile => (store) => {
 const profileFetchRequest = () => (store) => {
   const { token } = store.getState();
 
-  return superagent.get(`${API_URL}${route.PROFILE_ROUTE}/me`)
+  return superagent.get(`${API_URL}${routes.PROFILE_ROUTE}/me`)
     .set('Authorization', `Bearer ${token}`)
     .then((response) => {
       return store.dispatch(setProfile(response.body));
